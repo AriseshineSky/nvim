971ca3b37b289c242dd5e9ced29e6af172b52365
SmileintheSky <befruitful12@gmail.com>
2026-05-08 11:45:36 -0500

    change max line

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -108,7 +108,7 @@ return {
 			local tscontext = require("treesitter-context")
 			tscontext.setup({
 				enable = true,
-				max_lines = 0,
+				max_lines = 5,
 				min_window_height = 0,
 				line_numbers = true,
 				multiline_threshold = 20,
